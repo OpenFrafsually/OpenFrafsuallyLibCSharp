5cf28812dbab9a69ffb73d99d67b07800d359f92
AluminiumTech <14107316+AluminiumTech@users.noreply.github.com>
2020-12-08 21:49:15 +0000

    move files to src folder, update license notices, and update to 1.1.0

---
+++ src/DataSources/Implementation/FrapsDataSourceProvider.cs
@@ -0,0 +1,43 @@
+/*
+    Copyright (C) 2020 AluminiumTech
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+    USA
+ */
+
+using FrafsuallyLib.DataSources.Definition;
+
+using FrafsuallyLib.Models;
+
+namespace FrafsuallyLib.DataSources.Implementation
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class FrapsDataSourceProvider : IDataSourceProvider
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pathToFrapsFrameTimesFile"></param>
+        /// <returns></returns>
+        public FrameTime[] GetFrameTimesFromDataSource(string pathToFrapsFrameTimesFile)
+        {
+            CsvDataSourceProvider csvDataSourceProvider = new CsvDataSourceProvider();
+            return csvDataSourceProvider.GetDataFromColumns(pathToFrapsFrameTimesFile, "Time (ms)", "Frame");
+        }
+    }
+}
